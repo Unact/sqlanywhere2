af6679b31942b0aeed69b79d834eee7d0ce8bfc8
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2020-03-26 12:22:28 +0300

    Fix error invalid encoding

--- lib/sqlanywhere2/error.rb
+++ lib/sqlanywhere2/error.rb
@@ -2,13 +2,19 @@
 
 module SQLAnywhere2
   class Error < StandardError
+    ENCODE_OPTS = {
+      undef: :replace,
+      invalid: :replace,
+      replace: '?'
+    }
+
     attr_reader :error_number, :sql_state
 
     def initialize(msg, error_number = nil, sql_state = nil)
       @error_number = error_number
-      @sql_state = sql_state
+      @sql_state = sql_state ? sql_state.encode(**ENCODE_OPTS) : nil
 
-      super(msg)
+      super(msg.encode(**ENCODE_OPTS))
     end
   end
 end
--- spec/sqlanywhere2/error_spec.rb
+++ spec/sqlanywhere2/error_spec.rb
@@ -4,9 +4,11 @@ require './spec/spec_helper.rb'
 
 RSpec.describe SQLAnywhere2::Error do
   let!(:connection) { new_connection }
-  let!(:error) do
+  let(:test_query) { 'SUCH ERROR MUCH WOW' }
+
+  def create_error(query)
     begin
-      connection.execute_immediate('SUCH ERROR MUCH WOW')
+      connection.execute_immediate(query)
     rescue SQLAnywhere2::Error => e
       error = e
     end
@@ -15,7 +17,28 @@ RSpec.describe SQLAnywhere2::Error do
   end
 
   it 'responds to error_number and sql_state' do
+    error = create_error(test_query)
+
     expect(error).to respond_to(:error_number)
     expect(error).to respond_to(:sql_state)
+  end
+
+  context 'encoding' do
+    let(:invalid_message_encoding) { ['e5c67d1f'].pack('H*').force_encoding(connection.encoding) }
+    let(:valid_message_encoding) { '文字' }
+
+    it 'should form valid message for incorrectly encoded value' do
+      error = create_error("raiserror 55555 '#{invalid_message_encoding}'")
+
+      expect(error.message).to be_valid_encoding
+      expect(error.message.encoding.name).to eq(connection.encoding)
+    end
+
+    it 'should form valid message for correctly encoded value' do
+      error = create_error("raiserror 55555 '#{valid_message_encoding}'")
+
+      expect(error.message).to be_valid_encoding
+      expect(error.message.encoding.name).to eq(connection.encoding)
+    end
   end
 end
